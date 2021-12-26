afc8968fe960e5d80946e2fd6292f8b92091a358
Elina <elinaabdiakrimova@gmail.com>
2021-12-26 21:04:11 +0600

    fix

--- movie/search_urls.py
+++ movie/search_urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('', views.MovieSearchView.as_view(), name='search'),
-    path('<str:search_word>/', views.MovieSearchView.as_view(), name='search-find'),
-]
+# from django.urls import path
 #
+# from . import views
+#
+#
+# urlpatterns = [
+#     path('', views.MovieSearchView.as_view(), name='search'),
+#     path('<str:search_word>/', views.MovieSearchView.as_view(), name='search-find'),
+# ]
+# #
--- movie/urls.py
+++ movie/urls.py
@@ -1,16 +1,16 @@
-
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('review/delete/<int:pk>/', views.CommentDelete.as_view(), name='product_delete'),
-    path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
-    path("", views.MoviesView.as_view(), name="home"),
-    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
-    path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
-    # path("review/<int:pk>/", views.CommentDelete.as_view(), name="delete_product"),
-]
-
-
+#
+# from django.urls import path
+#
+# from . import views
+#
+#
+# urlpatterns = [
+#     path('review/delete/<int:pk>/', views.CommentDelete.as_view(), name='product_delete'),
+#     path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
+#     path("", views.MoviesView.as_view(), name="home"),
+#     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+#     path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
+#     # path("review/<int:pk>/", views.CommentDelete.as_view(), name="delete_product"),
+# ]
+#
+#
